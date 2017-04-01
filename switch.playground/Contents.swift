478a72b35e0caba6c7fd0ca23d13d00399677c0d
Eric Brooke <ericbrooke@mac.com>
2017-04-01 07:27:52 -0700

    chapter 5.40

--- switch.playground/Contents.swift
+++ switch.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import Cocoa
 
-var statusCode: Int = 404
+//var statusCode: Int = 404
 var errorString: String
 
 //switch statusCode {
@@ -32,18 +32,124 @@ errorString = "The requested failed"
 
 //ranges
 
-switch statusCode {
-case 100,101:
-    errorString += " informational, 1xx"
+//switch statusCode {
+//case 100,101:
+//    errorString += " informational, 1xx"
+//
+//case 204:
+//    errorString += " Succesful but no content, 204"
+//case 300...307:
+//    errorString += " Redircetion, 3xx"
+//case 400...417:
+//    errorString += " Client error, 4xx"
+//case 500...505:
+//    errorString += " Server erorr, 5xx"
+//default:
+//    errorString = "Unknown. Please review the request and try again"
+//}
 
-case 204:
-    errorString += " Succesful but no content, 204"
-case 300...307:
-    errorString += " Redircetion, 3xx"
-case 400...417:
-    errorString += " Client error, 4xx"
-case 500...505:
-    errorString += " Server erorr, 5xx"
+//value binding
+
+//switch statusCode {
+//case 100,101:
+//    errorString += " informational, \(statusCode)."
+//    
+//case 204:
+//    errorString += " Succesful but no content, 204"
+//case 300...307:
+//    errorString += " Redircetion, \(statusCode)."
+//case 400...417:
+//    errorString += " Client error, \(statusCode)."
+//case 500...505:
+//    errorString += " Server erorr, \(statusCode)."
+////case let unknownCode:
+////    errorString = "\(unknownCode) is not a known error code"
+//default:
+//    errorString = "\(statusCode) is not a known code."
+//}
+
+//where
+//statusCode = 204
+
+//switch statusCode {
+//case 100,101:
+//    errorString += " informational, \(statusCode)."
+//    
+//case 204:
+//    errorString += " Succesful but no content, 204"
+//case 300...307:
+//    errorString += " Redircetion, \(statusCode)."
+//case 400...417:
+//    errorString += " Client error, \(statusCode)."
+//case 500...505:
+//    errorString += " Server erorr, \(statusCode)."
+//    
+//case let unknownCode where (unknownCode >= 200 && unknownCode < 300) || unknownCode > 505:
+//    errorString = "\(unknownCode) is not a known error code"
+//default:
+//    errorString = "Unexpected error encountered"
+//}
+
+// tuples and pattern matching
+
+//var statusCode: Int = 418
+//
+//switch statusCode {
+//case 100,101:
+//    errorString += " informational, \(statusCode)."
+//
+//case 204:
+//    errorString += " Succesful but no content, 204"
+//case 300...307:
+//    errorString += " Redircetion, \(statusCode)."
+//case 400...417:
+//    errorString += " Client error, \(statusCode)."
+//case 500...505:
+//    errorString += " Server erorr, \(statusCode)."
+//
+//case let unknownCode where (unknownCode >= 200 && unknownCode < 300) || unknownCode > 505:
+//    errorString = "\(unknownCode) is not a known error code"
+//default:
+//    errorString = "Unexpected error encountered"
+//}
+
+//let error = (statusCode, errorString)
+//error.0
+//error.1
+
+//let error = (code: statusCode, error: errorString)
+//error.code
+//error.error
+
+let firstErrorCode = 404
+let secondErrorCode = 200
+let errorCodes = (firstErrorCode, secondErrorCode)
+
+switch errorCodes {
+case(404,406):
+        print("No items found.")
+case(404, _):
+    print("First item not found.")
+case(_, 404):
+    print("Second item not found.")
 default:
-    errorString = "Unknown. Please review the request and try again"
+    print("All items found.")
+}
+
+//switch vs if/else
+
+let age = 25
+//switch age {
+//case 18...35:
+//    print("Cool demographic")
+//default:
+//    break
+//}
+
+//if case 18...35 = age {
+//    print("Cool demographic")
+//}
+
+if case 18...35 = age, age >= 21 {
+    print("Cool demographic and of drinking age")
 }
